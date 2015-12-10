32ae4eef6533f462284cc3a26856ec1a596a4cb4
AntonioFettuccine <antoniofettuccine@yahoo.co.jp>
2015-12-10 23:17:17 +0900

    はじめての　コミット

---
+++ main/java/halloween/HalloweenItem.java
@@ -0,0 +1,41 @@
+package halloween;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+@Mod(modid="HalloweenItemCore", name="Halloween Item", version="1.0")
+
+public class HalloweenItem
+{
+	public static Item katsuage;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+
+		katsuage = new Item()
+		.setCreativeTab(CreativeTabs.tabTools)
+		.setUnlocalizedName("Trick or trade")
+		.setTextureName("samplemod:sample_item")
+		.setMaxStackSize(16);
+		GameRegistry.registerItem(katsuage, "Trick or trade");
+
+        GameRegistry.addRecipe(new ItemStack(HalloweenItem.katsuage),
+                "XXX",
+                "XYX",
+                "XXX",
+                'X',Blocks.pumpkin,
+                'Y',Items.emerald
+        );
+
+
+
+	}
+}
