b2da5280c5f6271ae7d532de71be2d7d856a26eb
AntonioFettuccine <antoniofettuccine@yahoo.co.jp>
2015-12-15 00:01:38 +0900

    村人→エメラルド×１ 参考：VillagerCannonMod  by defeatedcrow

--- main/java/halloween/HalloweenItemCore.java
+++ main/java/halloween/HalloweenItemCore.java
@@ -1,7 +1,10 @@
 package halloween;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -10,7 +13,25 @@ public class HalloweenItemCore extends Item {
     public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity)
     {
     	System.out.println("Happy Halloween!!");
+    	 if (entity != null && entity instanceof EntityVillager)
+         {
+         	if (stack.getItemDamage() == 0)
+         	{
+         		ItemStack dropItem = new ItemStack(Items.emerald);
+         		EntityItem drop = entity.entityDropItem(dropItem, 2.0F);
+         		drop.motionY += itemRand.nextFloat() * 0.05F;
+                 drop.motionX += (itemRand.nextFloat() - itemRand.nextFloat()) * 0.1F;
+                 drop.motionZ += (itemRand.nextFloat() - itemRand.nextFloat()) * 0.1F;
+
+                 if (!player.capabilities.isCreativeMode && --stack.stackSize <= 0)
+                 {
+                     player.inventory.setInventorySlotContents(player.inventory.currentItem, (ItemStack)null);
+                 }
+                 player.worldObj.playSoundAtEntity(player, "random.pop", 0.4F, 1.8F);
+                 entity.setDead();
+                 return true;
+         	}
+         }
         return false;
     }
-
-}
+ }
